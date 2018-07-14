d95889586e477f7013432c6d80a317ffbfd9bf49
Jerry <663794353@qq.com>
2018-07-15 01:55:01 +0900

    2018/7/15

--- assignment2_1.py
+++ assignment2_1.py
@@ -24,4 +24,37 @@ def np_sigmoid(z):
 
 
 np_x = np.array([1, 2, 3])
-print(np_sigmoid(np_x))
+print("sigmoid" + str(np_x) + ":" + str(np_sigmoid(np_x)))
+
+
+# derivative
+def np_sigmoid_derivative(z):
+    fx = 1 / (1 + np.exp(-z))
+    return fx * (1 - fx)
+
+
+print("sigmoid_derivative" + str(np_x) + ":" + str(np_sigmoid_derivative(np_x)))
+
+
+# np.shape np.reshape
+def image2vector(image):
+    """
+
+    :param image: a numpy array of shape(length height depth)
+    :return: a vector of shape(length*height*depth,1)
+    """
+    return image.reshape(image.shape[0] * image.shape[1] * image.shape[2], 1)
+
+
+np_matrix = np.array([[[0.67826139, 0.29380381],
+               [0.90714982, 0.52835647],
+               [0.4215251, 0.45017551]],
+
+              [[0.92814219, 0.96677647],
+               [0.85304703, 0.52351845],
+               [0.19981397, 0.27417313]],
+
+              [[0.60659855, 0.00533165],
+               [0.10820313, 0.49978937],
+               [0.34144279, 0.94630077]]])
+print("reshape:" + str(image2vector(np_matrix)))
