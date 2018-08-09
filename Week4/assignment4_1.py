d3fa32456a119b5b53695f7962bd37c2a9582bb6
Jerry <663794353@qq.com>
2018-08-09 23:25:04 +0900

    2018/8/09

---
+++ Week4/assignment4_1.py
@@ -0,0 +1,112 @@
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+from Week4.testCases_v3 import *
+from Week4.dnn_utils_v2 import sigmoid, sigmoid_backward, relu, relu_backward
+
+plt.rcParams['figure.figsize'] = (5.0, 4.0)  # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+np.random.seed(1)
+
+
+def initialize_parameters(n_x, n_h, n_y):
+    """
+    Argument:
+    n_x -- size of the input layer
+    n_h -- size of the hidden layer
+    n_y -- size of the output layer
+
+    Returns:
+    parameters -- python dictionary containing your parameters:
+                    W1 -- weight matrix of shape (n_h, n_x)
+                    b1 -- bias vector of shape (n_h, 1)
+                    W2 -- weight matrix of shape (n_y, n_h)
+                    b2 -- bias vector of shape (n_y, 1)
+    """
+    np.random.seed(1)
+    W1 = np.random.rand(n_h, n_x) * 0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.rand(n_y, n_h) * 0.01
+    b2 = np.zeros((n_y, 1))
+
+    assert (W1.shape == (n_h, n_x))
+    assert (b1.shape == (n_h, 1))
+    assert (W2.shape == (n_y, n_h))
+    assert (b2.shape == (n_y, 1))
+
+    parameters = {
+        "W1": W1,
+        "b1": b1,
+        "W2": W2,
+        "b2": b2
+    }
+
+    return parameters
+
+
+parameters = initialize_parameters(3, 2, 1)
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+print("------------------------------------------------------------------------------------------------")
+
+
+def initalize_parameters_deep(layer_dims):
+    """
+    Arguments:
+    layer_dims -- python array (list) containing the dimensions of each layer in our network
+
+    Returns:
+    parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
+                    Wl -- weight matrix of shape (layer_dims[l], layer_dims[l-1])
+                    bl -- bias vector of shape (layer_dims[l], 1)
+    """
+    np.random.seed(3)
+    parameters = {}
+    L = len(layer_dims)
+
+    for i in range(1, L):
+        parameters['W' + str(i)] = np.random.rand(layer_dims[i], layer_dims[i - 1])
+        parameters['b' + str(i)] = np.zeros((layer_dims[i], 1))
+
+        assert (parameters['W' + str(i)].shape == (layer_dims[i], layer_dims[i - 1]))
+        assert (parameters['b' + str(i)].shape == (layer_dims[i], 1))
+
+    return parameters
+
+
+parameters = initalize_parameters_deep([5, 4, 3])
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+
+
+def linear_forward(A, W, b):
+    """
+    Implement the linear part of a layer's forward propagation.
+
+    Arguments:
+    A -- activations from previous layer (or input data): (size of previous layer, number of examples)
+    W -- weights matrix: numpy array of shape (size of current layer, size of previous layer)
+    b -- bias vector, numpy array of shape (size of the current layer, 1)
+
+    Returns:
+    Z -- the input of the activation function, also called pre-activation parameter
+    cache -- a python dictionary containing "A", "W" and "b" ; stored for computing the backward pass efficiently
+    """
+    print("linear_forward - the shape of W is {} and the shape of A is {}".format(W.shape, A.shape))
+    Z = np.dot(W, A) + b
+
+    assert (Z.shape == (W.shape[0], A.shape[1]))
+    cache = (A, W, b)
+
+    return Z, cache
+
+
+A, W, b = linear_forward_test_case()
+Z, cache = linear_forward(A, W, b)
+print("Z = " + str(Z))
